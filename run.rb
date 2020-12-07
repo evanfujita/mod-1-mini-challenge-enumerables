c0a843ef672eaea7d425721c184d2dd07b85ebba
Evan <evanfujita@gmail.com>
2020-12-07 14:46:08 -0600

    Finished Lab

--- run.rb
+++ run.rb
@@ -7,31 +7,56 @@ spicy_foods = [
 ]
 
 def print_spicy_foods(spicy_foods)
-
+  spicy_foods.map do |item|
+    puts "#{item[:name]} (#{item[:cuisine]}) | Heat Level: #{item[:heat_level]}"
+  end
 end
 
 def get_names(spicy_foods)
-
+  array = []
+    spicy_foods.map do |item|
+      array << item[:name]
+    end
+    array
 end
 
-def spiciest_foods(spicy_foods)
-
+def spiciest_foods(spicy_foods)  
+  spicy_array = []
+  spicy_foods.collect do |hot_food| 
+    if hot_food[:heat_level] > 5
+      spicy_array << hot_food
+    end
+  end
+spicy_array
 end
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
-
+  spicy_specific = {}
+  spicy_foods.each do |menu|
+    if cuisine == menu[:cuisine]
+      spicy_specific = menu
+    end
+  end
+spicy_specific
 end
 
 # BONUS Deliverables
 def print_spiciest_foods(spicy_foods)
-
+  menu = spiciest_foods(spicy_foods)
+  print_spicy_foods(menu)
 end
 
 def average_heat_level(spicy_foods)
-
+  count = 0
+  average_spice = 0
+  spicy_foods.map do |item|
+    average_spice += item[:heat_level]
+    count += 1
+  end
+  average_spice /= count
 end
 
 # Use this to test your methods
 # run "ruby run.rb" and try calling the methods from the console
-binding.pry
+
 "pls"
